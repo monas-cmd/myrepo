0010c4e5af8dbee663671323ba22f9f8cb23954a
monas-cmd <monassirwm@google.com>
2022-10-21 18:39:34 +0300

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! Welcome to KOkir kitchen'
+    return 'Hello World! Welcome to KOkir kitchen last try'
 
 if __name__ == '__main__':
     app.run()
