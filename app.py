3a6f96d5671bed3c6140acd247a524df4115599e
monas-cmd <monassirwm@google.com>
2022-10-21 18:43:41 +0300

    added template

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! Welcome to KOkir kitchen last try'
-
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run()
