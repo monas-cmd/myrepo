c5f33fc3e8e27eeb2c64d685e394b654a3fae4f5
monas-cmd <monassirwm@google.com>
2022-10-21 19:21:33 +0300

    test

--- app.py
+++ app.py
@@ -7,10 +7,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+     myquery = { "location": "Urago 78" }
     z = []
     p = []
     for x in mycol.find():
         z.append(x)
-    return render_template('index.html', z = str(z) )
+    pokemons =[]
+    for x in mycou.find():
+        pokemons.append(x)
+    agg_result= mycol.aggregate( 
+    [{ 
+    "$group" :  
+        {"_id" : "null",  
+         "Total Subject" : {"$sum" :"$total" } 
+         }} 
+    ]) 
+    for i in agg_result: 
+        p.append(i)
+   
+    return render_template("dashboard.html",p = p, lenz = len(pokemons), pokemons = pokemons, len = len(z), z = z)
 if __name__ == '__main__':
     app.run()
